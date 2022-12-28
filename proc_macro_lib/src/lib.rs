f1b2fcfcc03110c3e947efc45db7d5bc490f6eb3
undefined <>
2022-12-28 04:29:19 +0100

    Remove old unused macros from utils. Move to lib

--- proc_macro_lib/src/lib.rs
+++ proc_macro_lib/src/lib.rs
@@ -606,3 +606,92 @@ pub fn impl_day_3(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     return proc_macro::TokenStream::from(stream);
 }
+
+
+
+// Some old macros removed from utils.rs, precursor to the above.
+// 
+// files (and folders?) are implicit modules(?)
+// "'mod'  looks for the 'foo' module in 'foo.rs' or 'foo/mod.rs'."
+// mod utils {
+// 
+// #[macro_export]
+// macro_rules! get_day {
+//     ($day:ident, $day2:ident) => {
+//         if include_optional::include_bytes_optional!("$day.rs").is_some() {Some(& $day :: $day2 {})} else {None}
+//     }
+// }
+// 
+// #[macro_export]
+// macro_rules! test_helper {
+//     (
+//         $fn:ident ($in1:expr, $out1:expr)
+//     ) => {
+//         $fn($in1, $out1);
+//     };
+//     (
+//         $fn:ident ( $in1:expr, $out1:expr, false )
+//     ) => {
+//         assert_eq!($in1, $out1);
+//     };
+// }
+// #[macro_export]
+// macro_rules! day {
+//     (
+//         $day:ident
+//         part1 |$input1:ident $(: &str)?| -> $answer_type1:ident $part1_impl:block
+//         part2 |$input2:ident $(: &str)?| -> $answer_type2:ident $part2_impl:block
+//         test1 $( ( $test1:tt, $($test1_tail:tt)+ ) $(,)? )*
+//         test2 $( ( $test2:tt, $($test2_tail:tt)+ ) $(,)? )*
+//     ) => {
+//         #[derive(Debug)]
+//         pub struct $day {}
+// 
+//         impl Day for $day {
+//             fn part1(&self, input: &str) {
+//                 println!("part1: {:?}", self.part1_impl(input) );
+//             }
+//             
+//             fn part2(&self, input: &str) {
+//                 println!("part2: {:?}", self.part2_impl(input) );
+//             }
+//         }
+//         
+//         impl $day {
+//             fn part1_impl(&self, $input1: &str) -> Result<$answer_type1, String> {
+//                 $part1_impl
+//             }
+// 
+//             fn part2_impl(&self, $input2: &str) -> Result<$answer_type1, String> {
+//                 $part2_impl
+//             }
+//         }
+//         
+//         #[cfg(test)]
+//         mod tests {
+//         use super::*;
+// 
+//         fn test1(s: &str, v: $answer_type1) {
+//             assert_eq!($day {}.part1_impl(s).unwrap(), v);
+//         }
+//         
+//         #[test]
+//         fn part1() {
+//             $(
+//                 crate::test_helper!( test1 ( $test1, $( $test1_tail )+ ) );
+//             )*
+//         }
+// 
+//         fn test2(s: &str, v: $answer_type2) {
+//             assert_eq!($day {}.part2_impl(s).unwrap(), v);
+//         }
+//         
+//         #[test]
+//         fn part2() {
+//             $(
+//                 crate::test_helper!( test2 ( $test2, $( $test2_tail )+ ) );
+//             )*
+//         }
+//         }
+//     };
+// }
--- src/utils.rs
+++ src/utils.rs
@@ -2,96 +2,11 @@
 // utility functions and macros
 
 use std::process;
-// use std::iter;
 
 use reqwest;
 use reqwest::header::COOKIE;
 use std::fs;
 
-// files (and folders?) are implicit modules(?)
-// "'mod'  looks for the 'foo' module in 'foo.rs' or 'foo/mod.rs'."
-// mod utils {
-
-// #[macro_export]
-// macro_rules! get_day {
-//     ($day:ident, $day2:ident) => {
-//         if include_optional::include_bytes_optional!("$day.rs").is_some() {Some(& $day :: $day2 {})} else {None}
-//     }
-// }
-
-// #[macro_export]
-// macro_rules! test_helper {
-//     (
-//         $fn:ident ($in1:expr, $out1:expr)
-//     ) => {
-//         $fn($in1, $out1);
-//     };
-//     (
-//         $fn:ident ( $in1:expr, $out1:expr, false )
-//     ) => {
-//         assert_eq!($in1, $out1);
-//     };
-// }
-// #[macro_export]
-// macro_rules! day {
-//     (
-//         $day:ident
-//         part1 |$input1:ident $(: &str)?| -> $answer_type1:ident $part1_impl:block
-//         part2 |$input2:ident $(: &str)?| -> $answer_type2:ident $part2_impl:block
-//         test1 $( ( $test1:tt, $($test1_tail:tt)+ ) $(,)? )*
-//         test2 $( ( $test2:tt, $($test2_tail:tt)+ ) $(,)? )*
-//     ) => {
-//         #[derive(Debug)]
-//         pub struct $day {}
-
-//         impl Day for $day {
-//             fn part1(&self, input: &str) {
-//                 println!("part1: {:?}", self.part1_impl(input) );
-//             }
-            
-//             fn part2(&self, input: &str) {
-//                 println!("part2: {:?}", self.part2_impl(input) );
-//             }
-//         }
-        
-//         impl $day {
-//             fn part1_impl(&self, $input1: &str) -> Result<$answer_type1, String> {
-//                 $part1_impl
-//             }
-
-//             fn part2_impl(&self, $input2: &str) -> Result<$answer_type1, String> {
-//                 $part2_impl
-//             }
-//         }
-        
-//         #[cfg(test)]
-//         mod tests {
-//         use super::*;
-
-//         fn test1(s: &str, v: $answer_type1) {
-//             assert_eq!($day {}.part1_impl(s).unwrap(), v);
-//         }
-        
-//         #[test]
-//         fn part1() {
-//             $(
-//                 crate::test_helper!( test1 ( $test1, $( $test1_tail )+ ) );
-//             )*
-//         }
-
-//         fn test2(s: &str, v: $answer_type2) {
-//             assert_eq!($day {}.part2_impl(s).unwrap(), v);
-//         }
-        
-//         #[test]
-//         fn part2() {
-//             $(
-//                 crate::test_helper!( test2 ( $test2, $( $test2_tail )+ ) );
-//             )*
-//         }
-//         }
-//     };
-// }
 
 pub fn hello_utils() {
     println!("Hello, utils!");
@@ -125,7 +40,7 @@ pub fn get_input_from_aoc(day: usize) -> String {
     // Cache input in a file.
     println!("Success! Caching input for day {} in a file", day);
     // match fs::write(path, &text) {
-    //     Ok(_) => todo(),// TODO success, do anything?
+    //     Ok(_) => todo(), // success, do anything?
     //     Err(e) => eprintln!("ERROR! Failed to cache input for day {}, with error: {}", day, e),
     // }
     if let Err(e) = fs::write(path, &text) {
