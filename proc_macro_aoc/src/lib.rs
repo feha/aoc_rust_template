47dbd91305e5896725f6aa47e72dce1af6148e8d
undefined <>
2022-12-30 01:13:06 +0100

    Fix accidental indentation

--- proc_macro_aoc/src/lib.rs
+++ proc_macro_aoc/src/lib.rs
@@ -77,78 +77,78 @@ pub fn instantiate_days(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
 }
 
 
-    #[derive(Debug, Default)]
-    struct DayParser {
-        parts: Vec<Ident>,
-    }
-    impl Parse for DayParser {
-        fn parse(input: ParseStream) -> syn::Result<Self> {
-            let mut day_parser = DayParser::default();
+#[derive(Debug, Default)]
+struct DayParser {
+    parts: Vec<Ident>,
+}
+impl Parse for DayParser {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mut day_parser = DayParser::default();
 
-            while !input.is_empty() {
-                let fn_ident = input.parse::<Ident>()?;
-                // Optional, Ok vs Err doesn't matter. Just consume if it exists.
-                input.parse::<syn::token::Comma>().ok();
-                day_parser.parts.push(fn_ident);
-            }
-
-            return Ok(day_parser);
+        while !input.is_empty() {
+            let fn_ident = input.parse::<Ident>()?;
+            // Optional, Ok vs Err doesn't matter. Just consume if it exists.
+            input.parse::<syn::token::Comma>().ok();
+            day_parser.parts.push(fn_ident);
         }
+
+        return Ok(day_parser);
     }
-    // A macro designed to look like a regular function call rather than a TokenStream,
-    // aiming to replace the smallest possible amount of code for the use it's designed for.
-    // It only expands to a `struct Day#` implementing `Day`,
-    // effectively linking the passed functions to main.rs
-    // as main.rs has macros importing & instantiating Day# structs for each Day#.rs file.
-    //
-    // Example:
-    // fn part1(_input: &str) -> Result<isize, String> {
-    //     return Ok(0);
-    // }
-    // ...
-    // impl_day!( part1, ... );
-    #[proc_macro]
-    pub fn impl_day(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-        let mut stream = TokenStream::new();
+}
+// A macro designed to look like a regular function call rather than a TokenStream,
+// aiming to replace the smallest possible amount of code for the use it's designed for.
+// It only expands to a `struct Day#` implementing `Day`,
+// effectively linking the passed functions to main.rs
+// as main.rs has macros importing & instantiating Day# structs for each Day#.rs file.
+//
+// Example:
+// fn part1(_input: &str) -> Result<isize, String> {
+//     return Ok(0);
+// }
+// ...
+// impl_day!( part1, ... );
+#[proc_macro]
+pub fn impl_day(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let mut stream = TokenStream::new();
 
-        let span = Span::call_site();
-        let binding = span.source_file().path();
-        let file = binding.to_str().unwrap();
-        let re = Regex::new(r".*day(\d+).rs").unwrap();
-        let caps = re.captures(file);
-        if let Some(caps) = caps {
-            let n: u32 = caps.get(1).unwrap().as_str().parse().unwrap();
-            let day_padded_upper = format!("Day{:0>2}", n).parse::<TokenStream>().unwrap();
+    let span = Span::call_site();
+    let binding = span.source_file().path();
+    let file = binding.to_str().unwrap();
+    let re = Regex::new(r".*day(\d+).rs").unwrap();
+    let caps = re.captures(file);
+    if let Some(caps) = caps {
+        let n: u32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let day_padded_upper = format!("Day{:0>2}", n).parse::<TokenStream>().unwrap();
 
-            let day_parser = syn::parse_macro_input!(input as DayParser);
+        let day_parser = syn::parse_macro_input!(input as DayParser);
 
-            let mut trait_parts = TokenStream::new();
-            for (k, fn_ident) in day_parser.parts.into_iter().enumerate() {
-                let k = k+1;
-                let trait_part_ident = format!("part_{}", k).parse::<TokenStream>().unwrap();
-                // let trait_part_ident = proc_macro::Ident::new(format!("part_{}", k).as_str(), span);
-                trait_parts.extend(quote!{
-                    fn #trait_part_ident(&self, input: &str) -> Result<String, ()> {
-                        return Ok(format!("Part {}: {:?}", #k, #fn_ident(input)));
-                    }
-                });
-            }
-
-            stream.extend(quote!{
-                #[derive(Debug)]
-                pub struct #day_padded_upper {}
-
-                impl Day for #day_padded_upper {
-                    #trait_parts
+        let mut trait_parts = TokenStream::new();
+        for (k, fn_ident) in day_parser.parts.into_iter().enumerate() {
+            let k = k+1;
+            let trait_part_ident = format!("part_{}", k).parse::<TokenStream>().unwrap();
+            // let trait_part_ident = proc_macro::Ident::new(format!("part_{}", k).as_str(), span);
+            trait_parts.extend(quote!{
+                fn #trait_part_ident(&self, input: &str) -> Result<String, ()> {
+                    return Ok(format!("Part {}: {:?}", #k, #fn_ident(input)));
                 }
             });
-
-        } else {
-            // don't generate anything
-            let str = format!("Tried to implement Day for a file with malformed name: file = \"{}\" , re = \"{:?}\"", file, re);
-            println!("{}", str);
-            // compile_error!(str); // can't figure out how to use these
         }
 
-        return proc_macro::TokenStream::from(stream);
+        stream.extend(quote!{
+            #[derive(Debug)]
+            pub struct #day_padded_upper {}
+
+            impl Day for #day_padded_upper {
+                #trait_parts
+            }
+        });
+
+    } else {
+        // don't generate anything
+        let str = format!("Tried to implement Day for a file with malformed name: file = \"{}\" , re = \"{:?}\"", file, re);
+        println!("{}", str);
+        // compile_error!(str); // can't figure out how to use these
     }
+
+    return proc_macro::TokenStream::from(stream);
+}
